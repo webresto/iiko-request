3c8512c35ea143d2dfe9506ad42c59bf55bbdc84
Ilya Dolmatov <dolmatoffilya@gmail.com>
2018-05-25 15:37:27 +0300

    Add post request

--- iiko-request.js
+++ iiko-request.js
@@ -5,134 +5,185 @@ documentation: https://docs.google.com/document/d/1kuhs94UV_0oUkI2CI3uOsNo_dydmh
 author: pub42 (xziy, ...)
 
 (c) 2018
-v 0.1
+v 0.2
 */
 var https = require('https'); // iiko biz support only SSL requests
 
 
-
-
-
 var config, access_token;
-exports.init = function(_config) {
-        config = _config; 
+exports.init = function (_config) {
+  config = _config;
 };
 ///////////////////////////////////////////////////////////////////////////////////
 
-exports.exec = function(method, data) {
-	return promise = new Promise(function(resolve, reject) {
-		switch(method.type) {
-		
-			case 'GET': 
-				  get(fetchGETurl(method, data));
-				break
+exports.call = function (method, params, modifier, data) {
+  return promise = new Promise(function (resolve, reject) {
+    switch (method.type) {
 
-			case 'POST':
-					// TODO: make POST request as GET analogie
-				break
+      case 'GET':
+        get(fetchGETurl(method, params, modifier)).then(
+          result => {
+            resolve(result);
+          },
+          error => {
+            reject(error);
+          });
+        break
 
-			default:
-					reject('Method is not defined');
-				break
-		}
+      case 'POST':
+        post(fetchGETurl(method, params, modifier), data).then(
+          result => {
+            resolve(result);
+          },
+          error => {
+            reject(error);
+          });
+        break
 
-	});
+      default:
+        reject('Method is not defined');
+        break
+    }
+
+  });
 };
 
 
+function fetchGETurl(method, data, modifier) {
+  let url = method.path;
+  if (modifier)
+    url = modifier(url);
+  url += '?';
 
-function fetchGETurl(method, data) {
-	let url = method.path+'?';
-
-	for (var param in method.params) {
-		if (!data.hasOwnProperty(param)){
-			console.error("iiko-request: The expected "+ param +" property is not found");		
-		} 
-		url = url + param '='+data[param]+'&'
-	}
-	return url
+  for (var param in method.params) {
+    if (!data.hasOwnProperty(param)) {
+      console.error("iiko-request: The expected " + param + " property is not found");
+    }
+    url = url + param + '=' + data[param] + '&'
+  }
+  return url
 }
 
 function get(url) {
-return  promise = new Promise(function(resolve, reject) {
-				let path = url + '&access_token=' + access_token;
-        checkToken().then(function(token) {
-                https.get({
-                    hostname: config.host,
-                    port: config.port,
-                    path: path,
-                    agent: false // create a new agent just for this one request
-                }, (res) => {
-                    let rawData = '';
-                    res.on('data', (data) => {
-                        rawData += data;
-                    });
-
-                    res.on('end', () => {
-                        try {
-                            data = JSON.parse(rawData);
-                            resolve(data);
-                        } catch (e) {
-                            //console.log(e.message);
-                        }
-                    });
-                });
-            }); // __checkToken
+  return promise = new Promise(function (resolve, reject) {
+    checkToken().then(function (token) {
+      let path = url + '&access_token=' + token;
+      console.log(path);
+      https.get({
+        hostname: config.host,
+        port: config.port,
+        path: path,
+        agent: false // create a new agent just for this one request
+      }, (res) => {
+        let rawData = '';
+        res.on('data', (data) => {
+          rawData += data;
         });
+
+        res.on('end', () => {
+          try {
+            const data = JSON.parse(rawData);
+            resolve(data);
+          } catch (e) {
+            console.log(rawData);
+            console.log(e.message);
+          }
+        });
+      });
+    });
+  });
 };
+
+function post(url, data) {
+  return promise = new Promise(function (resolve, reject) {
+    checkToken().then(function (token) {
+      let path = url + '&access_token=' + token;
+      console.log(path);
+      let req = https.request({
+        hostname: config.host,
+        port: config.port,
+        path: path,
+        agent: false,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }, (res) => {
+        res.setEncoding('utf8');
+        
+        let rawData = '';
+        res.on('data', (data) => {
+          rawData += data;
+        });
+
+        res.on('end', () => {
+          try {
+            const data = JSON.parse(rawData);
+            resolve(data);
+          } catch (e) {
+            console.log(rawData);
+            console.log(e.message);
+          }
+        });
+      });
+      req.write(JSON.stringify(data));
+      req.end();
+    });
+  });
+};
+
 ///////////////////////////////////////////////////////////////////////////////////
 
 function getToken() {
-    // Получение токена
-    //console.log(" IN __getToken");
-    let promise = new Promise(function(resolve, reject) {
-        let path = '/api/0/auth/access_token?user_id=' + config.login + '&user_secret=' + config.password;
-        https.get({
-            hostname: config.host,
-            port: config.port,
-            path: path,
-            agent: false // create a new agent just for this one request
-        }, (res) => {
+  // Получение токена
+  //console.log(" IN __getToken");
+  let promise = new Promise(function (resolve, reject) {
+    let path = '/api/0/auth/access_token?user_id=' + config.login + '&user_secret=' + config.password;
+    https.get({
+      hostname: config.host,
+      port: config.port,
+      path: path,
+      agent: false // create a new agent just for this one request
+    }, (res) => {
 
-            res.on('data', (token) => {
-                access_token = JSON.parse(token);
-                register.token = access_token
-                resolve(access_token);
-            });
-        });
+      res.on('data', (token) => {
+        access_token = JSON.parse(token);
+        resolve(access_token);
+      });
     });
-    return promise;
-    //console.log(" OUT __getToken");
+  });
+  return promise;
+  //console.log(" OUT __getToken");
 };
 
 ///////////////////////////////////////////////////////////////////////////////////
 function checkToken() {
-    // Проверяет токен, если токен нерабочий то получет токен
-    //console.log("IN __checkToken");
-    let promise = new Promise(function(resolve, reject) {
-        let path = '/api/0/auth/echo?msg=true&access_token=' + access_token;
-        //console.log(path);
-        https.get({
-            hostname: config.host,
-            port: config.port,
-            path: path,
-            agent: false // create a new agent just for this one request
-        }, (res) => {
+  // Проверяет токен, если токен нерабочий то получет токен
+  //console.log("IN __checkToken");
+  let promise = new Promise(function (resolve, reject) {
+    let path = '/api/0/auth/echo?msg=true&access_token=' + access_token;
+    //console.log(path);
+    https.get({
+      hostname: config.host,
+      port: config.port,
+      path: path,
+      agent: false // create a new agent just for this one request
+    }, (res) => {
 
-            res.on('data', (response) => {
-                if (response.toString() == '"Wrong access token"') {
-                    this.getToken().then(function(token) {
-                        resolve(token);
-                    });
-                }
+      res.on('data', (response) => {
+        if (response.toString() == '"Wrong access token"') {
+          getToken().then(function (token) {
+            resolve(token);
+          });
+        }
 
-                if (response.toString() == '"true"') {
-                    resolve(access_token);
-                }
-            });
-        });
+        if (response.toString() == '"true"') {
+          resolve(access_token);
+        }
+      });
     });
+  });
 
-    return promise
-    //console.log(" OUT  __checkToken");
+  return promise
+  //console.log(" OUT  __checkToken");
 };
